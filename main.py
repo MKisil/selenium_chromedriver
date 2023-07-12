452c5ed2a4df9dde9501d498e5513a380d8d58a1
Mikhailo Kisil <mihajlokisil20@gmail.com>
2023-07-12 11:16:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+If errors occur, try downloading the latest version of chromedriver.
+"""
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+
+
+def get_data_with_selenium(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument(f'user-agent={ua.random}')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    service = Service('./chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        driver.get(url)
+        with open('index.html', 'w', encoding='utf-8') as file:
+            file.write(driver.page_source)
+
+        time.sleep(5)
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+    driver.quit()
+
+
+if __name__ == '__main__':
+    get_data_with_selenium('https://www.google.com/')
